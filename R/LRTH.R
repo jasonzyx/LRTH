38c8820e034401524bb76df8f44c09c090d83638
jasonzyx <xuxx0284@umn.edu>
2016-02-28 21:25:50 -0600

    init new project

---
+++ R/LRTH.R
@@ -0,0 +1,47 @@
+#' The function to for the likelihood ratio test for genetic heteroneity
+#' 
+#' Take in genotypic score and disease status and return the p-value
+#' @param x: a n x 1 vector of genotypic score (i.e. 0, 1 or 2)
+#' @param y: a n x 1 vector of disease status; case/xcontrol: 1/0
+#' @return The asymptotic p-value of LRTH test
+#' @export
+
+
+LRTH = function(x,y){
+  ## implement Qian & Shao (2013) LRT test
+  ## given dat including: x (genotype: 0,1,2) and y(Case/control:1/0)
+  ## output p-value
+  dat = data.frame(y=y,x=x)
+  dat = as.data.frame(dat)
+  dat = dat[complete.cases(dat),]
+  n0 = nrow(subset(dat,y==1 & x==0))
+  n1 = nrow(subset(dat,y==1 & x==1))
+  n2 = nrow(subset(dat,y==1 & x==2))
+  m0 = nrow(subset(dat,y==0 & x==0))
+  m1 = nrow(subset(dat,y==0 & x==1))
+  m2 = nrow(subset(dat,y==0 & x==2))
+  n = n0+n1+n2
+  m = m0+m1+m2
+  
+  p0 = (n2+m2+(n1+m1)/2)/(n+m)
+  
+  logL0 = (m0+n0)*log((1-p0)^2) + (m1+n1)*log(2*p0*(1-p0)) + (m2+n2)*log(p0^2)
+  ph = (m2+m1/2)/m
+  
+  logLh = (m0)*log((1-ph)^2) + (m1)*log(2*ph*(1-ph)) + (m2)*log(ph^2)
+  
+  pd = (n2+n1/2)/n
+  
+  
+  logLd1 = (n0)*log(n0/n) + (n1)*log(n1/n) + (n2)*log(n2/n)
+  logLd2 = (n0)*log((1-pd)^2) + (n1)*log(2*pd*(1-pd)) + (n2)*log(pd^2)
+  if( (4*n0*n2) > (n1^2) ) logLd = logLd1 else logLd = logLd2
+  
+  
+  twolambda = 2*(logLd+logLh-logL0)
+  
+  pvalue = (pchisq(twolambda,1,lower.tail=F) + pchisq(twolambda,2,lower.tail=F))/2
+  
+  pvalue
+}
+
